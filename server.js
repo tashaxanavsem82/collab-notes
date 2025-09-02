e72d136eeafb8bcaf6fe1a010164f7b453e7fd03
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-02 11:42:22 -0500

    Fix deprecated mongoose options and update comments

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const DB_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/collab-notes';
 
 mongoose.connect(DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // Removed 'useCreateIndex' as it is deprecated in favor of indexes in MongoDB.
+    useUnifiedTopology: true
+    // Removed 'useCreateIndex' as it is deprecated in favor of application-managed indexes in the MongoDB driver.
 });
 
 app.get('/', (req, res) => {
