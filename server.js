4f9febcf2e6d5ba4fe8292a08ea38f7efd301069
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-02 11:33:34 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/collab-notes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
